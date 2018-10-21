691d116b2298e988fd0d5aa3b3f9ab7b85240b52
Volker <magicroomy@oge.de>
2018-10-21 19:27:13 +0200

    Button Handling, removal of unused VEML6075 lib

--- src/main.ino
+++ src/main.ino
@@ -24,81 +24,79 @@
 #define SDA 15
 #define SCK 4
 
-BME280Sensor *bmeSensor = new BME280Sensor() ;
-VEML6075Sensor *veml6075Sensor = new VEML6075Sensor() ;
-VL53L0XSensor *vl53l0xSensor = new VL53L0XSensor() ;
-CCS811Sensor *ccs811Sensor = new CCS811Sensor() ;
-MLX90614Sensor *mlx90614Sensor = new MLX90614Sensor() ;
-TCS34725Sensor *tcs34725Sensor = new TCS34725Sensor() ;
-VEML6040Sensor *veml6040Sensor = new VEML6040Sensor() ;
-Mics6814Sensor *mics6814Sensor = new Mics6814Sensor() ;
-I2CSWITCHSensor *channel0Sensor = new I2CSWITCHSensor(0) ;
-I2CSWITCHSensor *channel1Sensor = new I2CSWITCHSensor(1) ;
-BNO055Sensor *bno055Sensor = new BNO055Sensor() ;
+BME280Sensor *bmeSensor = new BME280Sensor();
+VEML6075Sensor *veml6075Sensor = new VEML6075Sensor();
+VL53L0XSensor *vl53l0xSensor = new VL53L0XSensor();
+CCS811Sensor *ccs811Sensor = new CCS811Sensor();
+MLX90614Sensor *mlx90614Sensor = new MLX90614Sensor();
+TCS34725Sensor *tcs34725Sensor = new TCS34725Sensor();
+VEML6040Sensor *veml6040Sensor = new VEML6040Sensor();
+Mics6814Sensor *mics6814Sensor = new Mics6814Sensor();
+I2CSWITCHSensor *channel0Sensor = new I2CSWITCHSensor(0);
+I2CSWITCHSensor *channel1Sensor = new I2CSWITCHSensor(1);
+BNO055Sensor *bno055Sensor = new BNO055Sensor();
 
-Sensor *sensorlist[] = { channel1Sensor, vl53l0xSensor, bmeSensor, veml6075Sensor, mlx90614Sensor, channel0Sensor, veml6040Sensor, ccs811Sensor, mics6814Sensor, bno055Sensor};
+Sensor *sensorlist[] = {channel1Sensor, vl53l0xSensor, bmeSensor, veml6075Sensor, mlx90614Sensor, channel0Sensor, veml6040Sensor, ccs811Sensor, mics6814Sensor, bno055Sensor};
 
-AmbientPage *ambientPage = new AmbientPage() ;
-LightPage *lightPage = new LightPage() ;
-BlankPage *blankPage = new BlankPage() ;
-GasesPage *gasesPage = new GasesPage() ;
-ExamplePage *examplePage = new ExamplePage() ;
-DistancePage *distancePage = new DistancePage() ;
-TemperaturePage *temperaturePage = new TemperaturePage() ;
-OrientationPage *orientationPage = new OrientationPage() ;
-Page *pageList[] = {ambientPage, lightPage, gasesPage, distancePage,temperaturePage, orientationPage} ;
+AmbientPage *ambientPage = new AmbientPage();
+LightPage *lightPage = new LightPage();
+BlankPage *blankPage = new BlankPage();
+GasesPage *gasesPage = new GasesPage();
+ExamplePage *examplePage = new ExamplePage();
+DistancePage *distancePage = new DistancePage();
+TemperaturePage *temperaturePage = new TemperaturePage();
+OrientationPage *orientationPage = new OrientationPage();
+Page *pageList[] = {ambientPage, lightPage, gasesPage, distancePage, temperaturePage, orientationPage};
 
-int currentPage = -1 ;
-int pages = 6 ;
+int currentPage = -1;
+int pages = 6;
 
 void initPages()
 {
-  ambientPage->setSensorData( bmeSensor->getTempSensorData(),
-  bmeSensor->getHumSensorData(),
-  bmeSensor->getPressSensorData(),
-  veml6040Sensor->getLuxSensorData(),
-  veml6075Sensor->getIndexSensorData()) ;
+  ambientPage->setSensorData(bmeSensor->getTempSensorData(),
+                             bmeSensor->getHumSensorData(),
+                             bmeSensor->getPressSensorData(),
+                             veml6040Sensor->getLuxSensorData(),
+                             veml6075Sensor->getIndexSensorData());
 
-  lightPage->setSensorData( veml6040Sensor->getRedSensorData(),
-  veml6040Sensor->getGreenSensorData(),
-  veml6040Sensor->getBlueSensorData(),
-  veml6040Sensor->getLuxSensorData(),
-  veml6040Sensor->getColorTempSensorData()) ;
+  lightPage->setSensorData(veml6040Sensor->getRedSensorData(),
+                           veml6040Sensor->getGreenSensorData(),
+                           veml6040Sensor->getBlueSensorData(),
+                           veml6040Sensor->getLuxSensorData(),
+                           veml6040Sensor->getColorTempSensorData());
 
   gasesPage->setSensorData(mics6814Sensor->getNH3SensorData(),
-  mics6814Sensor->getCOSensorData(),
-  mics6814Sensor->getNO2SensorData(),
-  mics6814Sensor->getC3H8SensorData(),
-  mics6814Sensor->getC4H10SensorData(),
-  mics6814Sensor->getCH4SensorData(),
-  mics6814Sensor->getH2SensorData(),
-  mics6814Sensor->getC2H5OHSensorData(),
-  ccs811Sensor->getCO2SensorData(),
-  ccs811Sensor->getVOCSensorData()
+                           mics6814Sensor->getCOSensorData(),
+                           mics6814Sensor->getNO2SensorData(),
+                           mics6814Sensor->getC3H8SensorData(),
+                           mics6814Sensor->getC4H10SensorData(),
+                           mics6814Sensor->getCH4SensorData(),
+                           mics6814Sensor->getH2SensorData(),
+                           mics6814Sensor->getC2H5OHSensorData(),
+                           ccs811Sensor->getCO2SensorData(),
+                           ccs811Sensor->getVOCSensorData()
 
-  ) ;
+  );
 
-  distancePage->setSensorData(vl53l0xSensor->getDistanceSensorData()) ;
-  temperaturePage->setSensorData(mlx90614Sensor->getTempSensorData()) ;
+  distancePage->setSensorData(vl53l0xSensor->getDistanceSensorData());
+  temperaturePage->setSensorData(mlx90614Sensor->getTempSensorData());
 
   orientationPage->setSensorData(
-    bno055Sensor->getRotationSensorData(), 
-    bno055Sensor->getRollSensorData(),
-    bno055Sensor->getPitchSensorData()
-     ) ;
-
+      bno055Sensor->getRotationSensorData(),
+      bno055Sensor->getRollSensorData(),
+      bno055Sensor->getPitchSensorData());
 }
 void displayUI()
 {
-  if ( currentPage >= 0)
+  if (currentPage >= 0)
   {
-    pageList[currentPage]->draw() ;
+    pageList[currentPage]->draw();
   }
 }
 
 void setup()
 {
-  Serial.begin(115200) ;
+  Serial.begin(115200);
   GO.begin();
   delay(200);
 
@@ -107,79 +105,85 @@ void setup()
   GO.Speaker.update();
 
   GO.lcd.setTextSize(2);
-  GO.lcd.setTextColor(WHITE,BLACK);
+  GO.lcd.setTextColor(WHITE, BLACK);
 
-  if(!SPIFFS.begin()){
+  if (!SPIFFS.begin())
+  {
     Serial.println("SPIFFS Mount Failed");
     return;
   }
 
-  GO.lcd.drawJpgFile(SPIFFS, "/Init.jpg", 0, 0, 320, 240, 0, 0, JPEG_DIV_NONE) ;
+  GO.lcd.drawJpgFile(SPIFFS, "/Init.jpg", 0, 0, 320, 240, 0, 0, JPEG_DIV_NONE);
 
   GO.lcd.setTextSize(3);
-  GO.lcd.setTextColor(CYAN,BLACK);
-  GO.lcd.drawString("INITIALIZING", 50, 100) ;
+  GO.lcd.setTextColor(CYAN, BLACK);
+  GO.lcd.drawString("INITIALIZING", 50, 100);
 
-//  Wire.setClock(100000)  ;
+  //  Wire.setClock(100000)  ;
   Wire.begin(SDA, SCK);
 
-  delay(40) ;
-  Serial.println("INIT Sensors") ;
+  delay(40);
+  Serial.println("INIT Sensors");
 
-  for ( int i = 0 ; i < (sizeof ( sensorlist) / sizeof (Sensor)) ; ++i)
+  for (int i = 0; i < (sizeof(sensorlist) / sizeof(Sensor)); ++i)
   {
-    Serial.printf("Init Sensor %d\n", i) ;
-    sensorlist[i]->begin() ;
-    Serial.printf("Init Sensor %d INITIALIZED\n", i) ;
-
+    Serial.printf("Init Sensor %d\n", i);
+    sensorlist[i]->begin();
+    Serial.printf("Init Sensor %d INITIALIZED\n", i);
   }
 
-  delay(40) ;
+  delay(40);
 
-  initPages() ;
+  initPages();
 
-
-  GO.lcd.drawJpgFile(SPIFFS, "/Init.jpg", 0, 0, 320, 240, 0, 0, JPEG_DIV_NONE) ;
+  GO.lcd.drawJpgFile(SPIFFS, "/Init.jpg", 0, 0, 320, 240, 0, 0, JPEG_DIV_NONE);
 
   GO.lcd.setTextSize(3);
-  GO.lcd.setTextColor(CYAN,BLACK);
-  GO.lcd.drawString("INIT COMPLETE", 50, 100) ;
+  GO.lcd.setTextColor(CYAN, BLACK);
+  GO.lcd.drawString("INIT COMPLETE", 50, 100);
 
-
-//  pageList[currentPage]->init() ;
+  //  pageList[currentPage]->init() ;
 }
+
+int slowdownCount = 0;
+
 void loop()
 {
 
-  for ( int i = 0 ; i < (sizeof ( sensorlist) / sizeof (Sensor)) ; ++i)
-  {
-    sensorlist[i]->update() ;
-  }
+  GO.BtnB.read();
+  GO.BtnA.read();
 
-  GO.BtnB.read() ;
-  GO.BtnA.read() ;
-
-  if ( GO.BtnA.isPressed() == 1)
+  if (GO.BtnA.isPressed() == 1)
   {
 
-    currentPage++ ;
+    currentPage++;
     if (currentPage == pages)
     {
-      currentPage = 0 ;
+      currentPage = 0;
     }
-    pageList[currentPage]->init() ;
+    pageList[currentPage]->init();
   }
 
-  if ( GO.BtnB.isPressed() == 1)
+  if (GO.BtnB.isPressed() == 1)
   {
-    currentPage-- ;
+    currentPage--;
     if (currentPage < 0)
     {
-      currentPage = pages-1 ;
+      currentPage = pages - 1;
     }
-    pageList[currentPage]->init() ;
+    pageList[currentPage]->init();
   }
 
-  displayUI();
-  delay(300) ;
+  slowdownCount++;
+  if (slowdownCount == 30)
+  {
+    slowdownCount = 0;
+    for (int i = 0; i < (sizeof(sensorlist) / sizeof(Sensor)); ++i)
+    {
+      sensorlist[i]->update();
+    }
+    displayUI();
+  }
+
+  delay(10);
 }
